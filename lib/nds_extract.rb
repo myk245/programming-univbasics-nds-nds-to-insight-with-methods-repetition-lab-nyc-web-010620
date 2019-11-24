d1fda801331df5a7ab304a69ac92ffb577ce4342
Cindy Kei <myk245@nyu.edu>
2019-11-24 23:48:50 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,10 +25,26 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
-  # Write this implementation
+  director_list = []
+  director_index = 0
+
+  while director_index < source.length do
+    director_list << source[director_index][:name]
+    director_index += 1
+  end
+  director_list
 end
 
 def total_gross(source)
+  total_gross_earnings = 0
+  director_index = 0
+
+  while director_index < source.length do
+    total_gross_earnings += gross_for_director( source[director_index] )
+    director_index += 1
+  end
+total_gross_earnings
+end
   # Write this implementation
   #
   # Should use methods:
@@ -38,6 +54,3 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-end
-
-
